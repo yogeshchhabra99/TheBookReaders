619b208169ac56a1d2a0d30420f5d258458c6103
Yogesh Chhabra <chhabra99yoesh@gmail.com>
2020-09-03 10:00:46 +0530

    cleaned

--- FrontEnd-PWA/js/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("sw.js")
-        .then((reg)=>console.log('Service worker registered', reg))
-        .catch((err)=> console.log('service worker not registered',err))
-}
